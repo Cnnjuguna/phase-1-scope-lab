63f26dbed95150868eba33a0c2f72b3227773e77
njugudev <njugudev@pop-os.localdomain>
2023-06-16 08:01:28 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+// const leastFavoriteCustomer = "mbwakni"
+
+
+function upperCaseCustomerName() {
+
+    return  customerName = customerName.toUpperCase()
+    
+}
+
+
+function setBestCustomer(){
+
+    bestCustomer = "not bob";
+    
+}    
+    
+
+
+function overwriteBestCustomer(){
+     bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = "Carlton"
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "avocado"
+    return leastFavoriteCustomer
+}
+
+console.log(changeLeastFavoriteCustomer())
